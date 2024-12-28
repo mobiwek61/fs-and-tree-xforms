337d2bc1547741c1753e79bf464121917dc93b56
desktop14 <sky99leaf@gmail.com>
2024-12-28 07:39:35 -0500

    add jpeg

--- imagesTreeCopyNResize.ts
+++ imagesTreeCopyNResize.ts
@@ -177,75 +177,75 @@ readAJSON('./public/jpeg').then(theArr => {
 
 /** ///////////////////// SAVE THIS!!! visual studio launch.json file for this project:
 {
-  // This file lives in the .vscode folder, which gets excluded from git by .gitignore because
-  //    it may have details about my computer.  
-  // ref: https://code.visualstudio.com/docs/editor/variables-reference
-  "version": "0.2.0",
-  "configurations": [
-    {
-      "name": "runSharpJS test",
-      "cwd": "${workspaceFolder}\\menuBuilder\\sharpTest",
-      "program": "runSharpJS.js",
-      "args": ["--imgFile", "American_Gothic88Wikimedia.jpg"],
-      "request": "launch",
-      "skipFiles": [
-        "<node_internals>/**"
-      ],
-      "type": "node"
-    },
-  
-    // that gets run as ../node_modules/typescript/bin/tsc --watch
-    // whenever the *.ts file changes, it gets transpiled into javascript so it can be run (see below)
-    // File tsconfig.json in the project tells tsc what to compile and how etc..
-    {
-      "name": "Transpile in watch mode",
-      "type": "node",
-      "request": "launch",
-      "runtimeExecutable": "tsc",
-      "cwd": "${workspaceFolder}\\menuBuilder", // run from this folder otherwise wont find ts file
-      "args": ["--watch"] // '--verbose' may only be used with '--build'
-      // refer to tsconfig.json for how it chooses *.ts to compile
-    },
-    {
-      // this runs the compiled .js file created from tsc transpiler above
-      "name": "treeToMenu",
-      "type": "node",
-      "request": "launch",
-      "skipFiles": [
-        "<node_internals>/**"
-      ],
-      // "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListInPublicJpeg.js",
-      "cwd": "${workspaceFolder}\\menuBuilder", // run from this folder; all paths now relative to here
-      // without cwd ->  "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListToMenuSpec_metaData.js",
-      "program": "compiledJS/treeToMenu.js",
-      "args": [
-        "--imgFolder", "../public/jpeg",
-        "--menuOut", "outputFiles/outMenuFile.json",
-        "--pictureMetaFile", "../public/pictureMetaData.json",
-        "--backupDir", "outputFiles/baks"
-        // moved to imageTreeCopyNResize.ts     "--shrunkImagesDir", "outputFiles/miniImages"
-        // slick but sloppy ->    "{ \"imgFolder\":\"./public/jpeg\", \"menuOut\":\"menuBuilder/output/outMenuFile.json\", \"pictureMetaFile\":\"menuBuilder/metaDataEdited//pictureMetaData.json\" }"
-      ]
-    },
-    {
-      // this runs the compiled .js file created from tsc transpiler above
-      "name": "imagesTreeCopyNResize",
-      //"console": "integratedTerminal", // allows for user input stdin for debug session
-      "type": "node",
-      "request": "launch",
-      "skipFiles": [
-        "<node_internals>/**"
-      ],
-      // "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListInPublicJpeg.js",
-      "cwd": "${workspaceFolder}\\menuBuilder", // run from this folder; all paths now relative to here
-      // without cwd ->  "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListToMenuSpec_metaData.js",
-      "program": "compiledJS/imagesTreeCopyNResize.js",
-      "args": [
-        "--imgSrcFolder", "../public/jpeg",
-        "--DRY_RUN", "false" // this disables interactive verification prompt
-        // slick but sloppy ->    "{ \"imgFolder\":\"./public/jpeg\", \"menuOut\":\"menuBuilder/output/outMenuFile.json\", \"pictureMetaFile\":\"menuBuilder/metaDataEdited//pictureMetaData.json\" }"
-      ]
-    }
-  ]
-}
+    // This file lives in the .vscode folder, which gets excluded from git by .gitignore because
+    //    it may have details about my computer.  
+    // ref: https://code.visualstudio.com/docs/editor/variables-reference
+    "version": "0.2.0",
+    "configurations": [
+      {
+        "name": "runSharpJS test",
+        "cwd": "${workspaceFolder}\\menuBuilder\\sharpTest",
+        "program": "runSharpJS.js",
+        "args": ["--imgFile", "American_Gothic88Wikimedia.jpg"],
+        "request": "launch",
+        "skipFiles": [
+          "<node_internals>/**"
+        ],
+        "type": "node"
+      },
+    
+      // that gets run as ../node_modules/typescript/bin/tsc --watch
+      // whenever the *.ts file changes, it gets transpiled into javascript so it can be run (see below)
+      // File tsconfig.json in the project tells tsc what to compile and how etc..
+      {
+        "name": "Transpile in watch mode",
+        "type": "node",
+        "request": "launch",
+        "runtimeExecutable": "tsc",
+        "cwd": "${workspaceFolder}\\menuBuilder", // run from this folder otherwise wont find ts file
+        "args": ["--watch"] // '--verbose' may only be used with '--build'
+        // refer to tsconfig.json for how it chooses *.ts to compile
+      },
+      {
+        // this runs the compiled .js file created from tsc transpiler above
+        "name": "treeToMenu",
+        "type": "node",
+        "request": "launch",
+        "skipFiles": [
+          "<node_internals>/**"
+        ],
+        // "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListInPublicJpeg.js",
+        "cwd": "${workspaceFolder}\\menuBuilder", // run from this folder; all paths now relative to here
+        // without cwd ->  "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListToMenuSpec_metaData.js",
+        "program": "compiledJS/treeToMenu.js",
+        "args": [
+          "--imgFolder", "../public/jpeg",
+          "--menuOut", "outputFiles/outMenuFile.json",
+          "--pictureMetaFile", "../public/pictureMetaData.json",
+          "--backupDir", "outputFiles/baks"
+          // moved to imageTreeCopyNResize.ts     "--shrunkImagesDir", "outputFiles/miniImages"
+          // slick but sloppy ->    "{ \"imgFolder\":\"./public/jpeg\", \"menuOut\":\"menuBuilder/output/outMenuFile.json\", \"pictureMetaFile\":\"menuBuilder/metaDataEdited//pictureMetaData.json\" }"
+        ]
+      },
+      {
+        // this runs the compiled .js file created from tsc transpiler above
+        "name": "imagesTreeCopyNResize",
+        //"console": "integratedTerminal", // allows for user input stdin for debug session
+        "type": "node",
+        "request": "launch",
+        "skipFiles": [
+          "<node_internals>/**"
+        ],
+        // "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListInPublicJpeg.js",
+        //"cwd": "${workspaceFolder}\\menuBuilder", // run from this folder; all paths now relative to here
+        // without cwd ->  "program": "${workspaceFolder}\\menuBuilder\\compiledJS\\fileListToMenuSpec_metaData.js",
+        "program": "compiledJS/imagesTreeCopyNResize.js",
+        "args": [
+          "--imgSrcFolder", "./origJPEG",
+          "--DRY_RUN", "false" // this disables interactive verification prompt
+          // slick but sloppy ->    "{ \"imgFolder\":\"./public/jpeg\", \"menuOut\":\"menuBuilder/output/outMenuFile.json\", \"pictureMetaFile\":\"menuBuilder/metaDataEdited//pictureMetaData.json\" }"
+        ]
+      }
+    ]
+  }
 ///////////// END!!! visual studio launch.json */
