619a4ad8e2dfdd6420cf76e64b6b18f259a95ad2
desktop14 <mobiwek61@gmail.com>
2025-02-23 12:52:43 -0500

    aa

--- treeCopyFilesResizedAndExifMods/imagesTreeCopyNResize.sh
+++ treeCopyFilesResizedAndExifMods/imagesTreeCopyNResize.sh
@@ -3,12 +3,17 @@
 compileCmd='../node_modules/typescript/bin/tsc --project code/tsconfig_dogs.json --watch'
 COMPILED_TS_TO_JS_FILE='code/typescriptCompiled/imagesTreeCopyNResize.js'
 
-if !(test -f "$COMPILED_TS_TO_JS_FILE"); then
-  echo runnable file: $COMPILED_TS_TO_JS_FILE " not found. "
-  echo 'to compile *.ts to *.js, run this command:'
-  echo $compileCmd
-  exit;
-fi
+echo starting tsc compiliation of *.ts to *.js
+../node_modules/typescript/bin/tsc --project code/tsconfig_dogs.json  #compile-every-time
+echo compilation done
+####### uncomment below if compile-every-time is not used
+# if !(test -f "$COMPILED_TS_TO_JS_FILE"); then
+#   echo runnable file: $COMPILED_TS_TO_JS_FILE " not found. "
+#   echo 'to compile *.ts to *.js, run this command:'
+#   echo $compileCmd
+#   exit;
+# fi
+######
 
 commd="node.exe $COMPILED_TS_TO_JS_FILE --imgSrcFolder jpegs/origJPEG --PIXMINI 15 --PIXFULL 445 --DRY_RUN false"
 
@@ -18,10 +23,12 @@ commd="node.exe $COMPILED_TS_TO_JS_FILE --imgSrcFolder jpegs/origJPEG --PIXMINI 
 echo -------------------------------------
 # now run it
 eval $commd
-echo $(tput setaf 1)!!!! NOTICE: after modifying .ts source code, compile with this command: $(tput sgr0)
-echo -n $(tput setaf 2) # this sets color GREEN
-echo $compileCmd
-echo -n $(tput sgr0) # this resets color
-echo above stays in the window and recompiles .ts files whenever modified
 
+##### uncomment below if compile-every-time is not used
+# echo $(tput setaf 1)!!!! NOTICE: after modifying .ts source code, compile with this command: $(tput sgr0)
+# echo -n $(tput setaf 2) # this sets color GREEN
+# echo $compileCmd
+# echo -n $(tput sgr0) # this resets color
+# echo above stays in the window and recompiles .ts files whenever modified
+######
 
